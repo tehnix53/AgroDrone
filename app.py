2ff14c2f392ddfd760dbc3d19f0756b0eaca52b8
alex <tehnix53@gmail.com>
2021-01-18 15:48:02 +0200

    app run

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def drone_upload():
     return render_template('upload.html')
 
 
-app.run('127.0.0.1', 5000, debug=True)
+#app.run('127.0.0.1', 5000, debug=True)
 
 
-#if __name__ == '__main__':
- #   app.run()
+if __name__ == '__main__':
+    app.run()
